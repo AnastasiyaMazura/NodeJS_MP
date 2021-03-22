70152eb03eb27f652c96cf4afd95693cda564369
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2021-03-22 23:00:09 +0300

    Update task3

---
+++ src/task3.js
@@ -0,0 +1,29 @@
+import csv from 'csvtojson';
+import fs from 'fs';
+import { pipeline } from 'stream';
+
+const csvFilePath = './csv/nodejs-hw1-ex1.csv';
+const fileToWrite = './csv/file.txt';
+
+const readStream = fs.createReadStream(csvFilePath);
+const writeSyream = fs.createWriteStream(fileToWrite);
+
+readStream.on("error", (err) => {
+    console.log('file read error');
+});
+writeSyream.on("error", (err) => {
+    console.log('file write error');
+});
+
+pipeline(
+    readStream,
+    csv(),
+    writeSyream,
+    (err) => {
+        if (err) {
+            console.error('Pipeline failed.', err);
+        } else {
+            console.log('Pipeline succeeded.');
+        }
+    }
+);
