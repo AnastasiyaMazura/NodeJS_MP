e8e75d9b3b331e564a741df1baa96c0c50210e6a
AnastasiyaMazura <mazura.anastasiya@gmail.com>
2021-04-05 18:45:13 +0300

    Home work 2 fix

--- src/task1.js
+++ src/task1.js
@@ -2,5 +2,5 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.resume();
 process.stdin.on('data', (inputData) => {
-    return process.stdout.write(inputData.split('').reverse().join('') + '\n\n');
+    return process.stdout.write(`${inputData.split('').reverse().join('')  }\n\n`);
 });
